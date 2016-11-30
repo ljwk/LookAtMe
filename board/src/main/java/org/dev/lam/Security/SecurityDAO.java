da45cc7eb0a93f0a67d54e250dd062a5888eff19
digh1219 <digh1219@gmail.com>
2016-11-30 15:51:21 +0900

    idontknow

--- board/src/main/java/org/dev/lam/Security/SecurityDAO.java
+++ board/src/main/java/org/dev/lam/Security/SecurityDAO.java
@@ -14,7 +14,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SecurityDAO {
 	private Connection conn;
 	private PreparedStatement pstmt;
