71fbcf61eb89be4fb4ba4c7a10d7fa0cdec3b4d8
duniv015 <duniv015@duniv015-PC>
2016-12-02 12:16:33 +0900

    ddd

--- board/src/main/java/org/dev/lam/Board/BoardController.java
+++ board/src/main/java/org/dev/lam/Board/BoardController.java
@@ -133,6 +133,8 @@ public class BoardController {
 		return "board/readd";
 	}
 
+
+
 	@RequestMapping(value = "/reple", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, String> re(BoardVO board) {
