0f1ce24e0b7b36fd324a93cf94b20dd5f956639d
duniv010 <duniv010@duniv010-PC>
2016-12-02 10:21:50 +0900

    gg

---
+++ board/src/main/java/org/dev/lam/Security/SecurityVO.java
@@ -0,0 +1,32 @@
+package org.dev.lam.Security;
+
+public class SecurityVO {
+	private String id;
+	private String pwd;
+	private String authority;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+}
