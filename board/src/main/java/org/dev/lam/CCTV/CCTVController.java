314ea13d33fb92250059843e1e604b6d912b789f
digh1 <digh1@DESKTOP-H8MFDDO>
2016-12-04 17:18:02 +0900

    testcode move

--- board/src/main/java/org/dev/lam/CCTV/CCTVController.java
+++ board/src/main/java/org/dev/lam/CCTV/CCTVController.java
@@ -1,5 +1,15 @@
 package org.dev.lam.CCTV;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,4 +22,74 @@ public class CCTVController {
 	public String cctv(Model model) {
 		return "cctv/cctv";
 	}
+	
+	@RequestMapping(value = "vv")
+	public String vv(){
+		return "cctv/view";
+	}
+	
+	@RequestMapping(value = "/test")
+	public void image() {
+		DataInputStream input = null;
+		Socket socket = null;
+		ServerSocket serverSocket = null;
+		
+		try {
+			serverSocket = new ServerSocket(8088);
+//			serverSocket.setSoTimeout(1000 /* milliseconds */);
+			while (true) {
+				do {
+					try {
+						System.out.println("¥Î±‚");
+						socket = serverSocket.accept();
+					} catch (final SocketTimeoutException e) {
+						System.out.println("è≥");
+					}
+				} while (socket == null);
+
+				System.out.println(socket);
+
+				URL url = new URL("http://192.168.2.26:8083/");
+				URLConnection getconn = url.openConnection();
+				input = new DataInputStream(getconn.getInputStream());
+
+				imageThread(socket, input);
+				socket = null;
+				// serverSocket.close();
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void imageThread(Socket socket, DataInputStream input) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					DataOutputStream output = null;
+					output = new DataOutputStream(socket.getOutputStream());
+					
+					while (true) {
+						byte by= input.readByte();
+						System.out.println(input.readByte());
+						output.write(by);
+						output.flush();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						socket.close();
+						input.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+	
 }
